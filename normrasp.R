e026fa3eb01a1243f7e5982e7e35006958ab52bc
Никита <71729237+Nikita260396@users.noreply.github.com>
2020-11-27 19:24:21 +0300

    Update normrasp.R

--- normrasp.R
+++ normrasp.R
@@ -1,14 +1,14 @@
 normrasp<-function(n,mean_x,sd_x,mean_e,sd_e)
 {## n - размер выборки, mean_x,sd_x - среднее и дисперсия x,mean_e,sd_e - среднее и дисперсия e.
-  set.seed(6)
-  x<-rnorm(n,mean_x,sd_x)
-  e<-rnorm(n,mean_e,sd_e)
-  y<-c(100-12*x+e)
-  x
-  ##png("picture.png")##файл с графиками сохраняется
-  layout(matrix(c(2,1,0,3),2,2,byrow = T))
-  plot(x,y)
-  boxplot(x)
-  boxplot(y,horizontal = T)
-  ##dev.off()
+set.seed(6)
+x<-rnorm(300,12,2)
+e<-rnorm(300,0,15)
+y<-c(100-12*x+e)
+x
+##png("picture.png")##файл с графиками сохраняется
+layout(matrix(c(2,1,1,2,1,1,0,3,3),3,3, byrow = T))
+plot(x,y)
+boxplot(x)
+boxplot(y,horizontal = T)
+##dev.off()
 }
